ed34b760403d04c8f4dfe7ed5095c02e6cb72cf7
Jaehong Choi <hong83@gmail.com>
2016-02-19 16:01:43 +0900

    BDD-344 produce the final tar archive after preparation

--- prepare_druid.sh
+++ prepare_druid.sh
@@ -5,6 +5,8 @@ target_dir=${root_dir}/distribution/target
 git_revision=`git rev-parse HEAD`
 git_origin=`git config --get remote.origin.url`
 date=`LC_ALL=C date`
+druid_release_dir="${druid_release}.${git_revision}"
+druid_release_tar="${druid_release_dir}-bin.tar.gz"
 
 rm -rf ${target_dir}/${druid_release}
 
@@ -22,3 +24,8 @@ cp -a ./*.sh ${target_dir}/${druid_release}
 cd ${target_dir}
 tar xvzf mysql-metadata-storage-bin.tar.gz
 cp -a ./mysql-metadata-storage ${druid_release}/extensions/
+rm "${druid_release_tar}"
+mv ${druid_release} ${druid_release_dir}
+tar cvzf "${druid_release_tar}" ${druid_release_dir}
+mv ${druid_release_dir} ${druid_release}
+cp -f "${druid_release_tar}" ${root_dir}
