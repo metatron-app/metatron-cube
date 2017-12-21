7c35e5cb39bb70abb0d3a845fcb5f3fc34a8fa9b
navis.ryu <navis@apache.org>
2017-12-22 00:39:05 +0900

    Log possible jackson version conflicts

--- processing/src/main/java/io/druid/guice/GuiceInjectors.java
+++ processing/src/main/java/io/druid/guice/GuiceInjectors.java
@@ -25,17 +25,24 @@ import com.google.inject.Binder;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
-import io.druid.jackson.FunctionInitializer;
+import com.metamx.common.logger.Logger;
 import io.druid.jackson.JacksonModule;
 
+import java.net.URI;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  */
 public class GuiceInjectors
 {
+  private static final Logger log = new Logger(GuiceInjectors.class);
+
   public static Collection<Module> makeDefaultStartupModules(String... propertiesLocations)
   {
     return ImmutableList.<Module>of(
@@ -57,7 +64,38 @@ public class GuiceInjectors
 
   public static Injector makeStartupInjector(String... propertiesLocations)
   {
+    String[] version1 = findResources(
+        "com/fasterxml/jackson/databind/deser/std/FromStringDeserializer.class",
+        "jackson-databind"
+    );
+    String[] version2 = findResources(
+        "com/fasterxml/jackson/datatype/guava/deser/HostAndPortDeserializer.class",
+        "jackson-datatype-guava"
+    );
+    if (!Objects.equals(version1[1], version2[1])) {
+      log.warn("jackson-databind is in '%s' and jackson-datatype-guava is in '%s'", version1[0], version1[1]);
+      log.warn("Possible dependency conflicts.. consider using 'mapreduce.job.user.classpath.first=true'");
+    }
     return Guice.createInjector(makeDefaultStartupModules(propertiesLocations));
+  }
+
+  // log message for xxxxing HostAndPortDeserializer problem
+  private static String[] findResources(String resource, String library)
+  {
+    URL found = Guice.class.getClassLoader().getResource(resource);
+    if (found != null && "jar".equals(found.getProtocol())) {
+      String path = URI.create(found.getPath()).getPath();
+      if (path.indexOf('!') > 0) {
+        path = path.substring(0, path.indexOf('!'));
+      }
+      String regex = ".*" + library + "-(.+)\\.jar";
+      Matcher matcher = Pattern.compile(regex).matcher(path);
+      if (matcher.matches()) {
+        return new String[] {path, matcher.group(1)};
+      }
+      return new String[] {path, null};
+    }
+    return new String[] {found == null ? "" : found.getPath(), null};
   }
 
   public static Injector makeStartupInjectorWithModules(Iterable<? extends Module> modules)
