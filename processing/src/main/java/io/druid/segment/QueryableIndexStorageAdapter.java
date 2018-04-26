7fee9dfc82208045d34fc0214a27fc5967920b06
navis.ryu <navis@apache.org>
2018-04-26 09:05:09 +0900

    Use local resolver instead of merged one in filter construction

--- processing/src/main/java/io/druid/segment/QueryableIndexStorageAdapter.java
+++ processing/src/main/java/io/druid/segment/QueryableIndexStorageAdapter.java
@@ -255,7 +255,8 @@ public class QueryableIndexStorageAdapter implements StorageAdapter
 
     final BitmapFactory bitmapFactory = index.getBitmapFactoryForDimensions();
 
-    final DimFilter[] filters = Filters.partitionWithBitmapSupport(filter, resolver);
+    final RowResolver localResolver = RowResolver.of(index, resolver.getVirtualColumns());
+    final DimFilter[] filters = Filters.partitionWithBitmapSupport(filter, localResolver);
 
     final DimFilter bitmapFilter = filters == null ? null : filters[0];
     final DimFilter valuesFilter = filters == null ? null : filters[1];
