a16ee7be2b276e8811e05972237b980db2ca1fdb
navis.ryu <navis@apache.org>
2022-05-02 14:26:56 +0900

    #4038 QueryEvent is not jackson serializable

--- java-util/src/main/java/io/druid/java/util/emitter/service/QueryEvent.java
+++ java-util/src/main/java/io/druid/java/util/emitter/service/QueryEvent.java
@@ -1,5 +1,20 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.druid.java.util.emitter.service;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import io.druid.java.util.emitter.core.Event;
@@ -17,8 +32,8 @@ public class QueryEvent implements Event
   private final Long timeNs;
   private final Long bytes;
   private final Integer rows;
-  private String exception;
-  private String interrupted;
+  private final String exception;
+  private final String interrupted;
 
   public QueryEvent(
       DateTime createdTime,
@@ -72,46 +87,52 @@ public class QueryEvent implements Event
     return createdTime;
   }
 
-  public String getSuccess() {
+  public String getSuccess()
+  {
     return success;
   }
 
-  public Long getTimeNs() {
+  public Long getTimeNs()
+  {
     return timeNs;
   }
 
-  public Long getBytes() {
+  public Long getBytes()
+  {
     return bytes;
   }
 
-  public Integer getRows() {
+  public Integer getRows()
+  {
     return rows;
   }
 
-  public String getException() {
+  public String getException()
+  {
     return exception;
   }
 
-  public String getInterrupted() {
+  public String getInterrupted()
+  {
     return interrupted;
   }
 
   @Override
+  @JsonValue
   public Map<String, Object> toMap()
   {
     return ImmutableMap.<String, Object>builder()
-            .put("feed", getFeed())
-            .put("timestamp", createdTime.toString())
-            .put("id", id)
-            .put("remoteAddress", Strings.nullToEmpty(remoteAddress))
-            .put("query", query)
-            .put("success", success)
-            .put("querytime", timeNs)
-            .put("bytes", bytes)
-            .put("rows", rows)
-            .put("exception", exception)
-            .put("interrupted", interrupted)
-            .build();
+                       .put("feed", getFeed())
+                       .put("timestamp", createdTime.toString())
+                       .put("id", id)
+                       .put("remoteAddress", Strings.nullToEmpty(remoteAddress))
+                       .put("query", query)
+                       .put("success", success)
+                       .put("querytime", timeNs)
+                       .put("bytes", bytes)
+                       .put("rows", rows)
+                       .put("exception", exception)
+                       .put("interrupted", interrupted)
+                       .build();
   }
-
 }
